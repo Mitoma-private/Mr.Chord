6348a1a318133d78e94d9d0516b2282d88b8ec01
Mitoma-private <mitoayu1106@ezweb.ne.jp>
2025-05-07 10:50:19 +0000

    dd

--- main.py
+++ main.py
@@ -17,23 +17,24 @@ if 'page_control' not in st.session_state:
 left, right = st.columns([3,1])
 l_button, c_button, r_button = st.columns(3)
 
-left.text("tintin")
-
 ##ボタン押下時の処理
 if r_button.button("次へ"):
     st.session_state['page_control'] += 1
 if  r_button.button("リセット"):
     st.session_state['page_control'] = 0
-    
+
 if st.session_state['page_control'] == 0:
+    right.image(config.default, caption="コードさん", width=200) 
+
+if st.session_state['page_control'] == 1:
     left.subheader("やぁ! 私の名前はコードさんだよ!")
     right.image(config.happy, caption="コードさん", width=200)
 
-if st.session_state['page_control'] == 1:
-    left.subheader("今から曲あてクイズをやってみよう!")
-    right.image(config.default, caption="こーどさん", width=200)
-
 if st.session_state['page_control'] == 2:
+    left.subheader("今から曲あてクイズをやってみよう!")
+    right.image(config.default, caption="コードさん", width=200)
+
+if st.session_state['page_control'] == 3:
     left.subheader("15曲の中から好きな曲を選んでね!")
     right.image(config.default, caption="コードさん", width=200)
 
