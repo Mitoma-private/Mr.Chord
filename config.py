52a39e739e69ad7910b39710e8db49ce7a79bea0
Mitoma-private <mitoayu1106@ezweb.ne.jp>
2025-05-07 10:21:09 +0000

    ww

---
+++ config.py
@@ -0,0 +1,9 @@
+from PIL import Image
+
+##画像ファイル
+default = Image.open("./figure/default.png")
+happy = Image.open("./figure/happy.png")
+learning = Image.open("./figure/learning.png")
+sad = Image.open("./figure/sad.png")
+
+##音声ファイル
--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import streamlit as st
 import numpy as np
-from PIL import Image
 import pandas as pd
+import streamlit.components.v1 as stc
+import time
 
+##タイトル
 st.title("曲当てロボット コードくん")
 st.text("コードくんはピアノ音源を読み込ませることでそれがなんの曲かを当てることができるよ！")
 
-default = Image.open("./figure/default.png")
-happy = Image.open("./figure/happy.png")
-learning = Image.open("./figure/learning.png")
-sad = Image.open("./figure/sad.png")
+
 
 left, right = st.columns([3,1])
 right.image(default, caption="コードくん", width=200)
@@ -35,4 +34,5 @@ data_df = pd.DataFrame(
     }
 )
 left.dataframe(data_df)
+
 
