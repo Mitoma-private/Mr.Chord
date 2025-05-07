54b44759aa31e36a9c1aa4aaab5e15e8870c5c32
Mitoma-private <mitoayu1106@ezweb.ne.jp>
2025-05-07 07:04:08 +0000

    bb

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 from PIL import Image
 
 st.title("曲当てロボット コードくん")
-st.text("コードくんはピアノを引いた音源を読み込ませることでそれがなんの曲かを当てることができるよ！")
+st.text("コードくんはピアノ音源を読み込ませることでそれがなんの曲かを当てることができるよ！")
 
 default = Image.open("./figure/default.png")
 happy = Image.open("./figure/happy.png")
@@ -12,5 +12,5 @@ sad = Image.open("./figure/sad.png")
 
 left, right = st.columns([3,1])
 right.image(default, caption="コードくん", width=200)
-left.subheader("①15曲の中から好きな曲を選んでみよう!")
+left.subheader("①15曲の中から好きな曲を選んでね!")
 
