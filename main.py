49f894c16a53348c47e1204fc73c8341ba9c17cc
Mitoma-private <mitoayu1106@ezweb.ne.jp>
2025-05-07 06:41:11 +0000

    bb

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import streamlit as st
 import numpy as np
+from PIL import Image
 
 st.title("曲当てロボット コードくん")
 st.text("コードくんはピアノを引いた音源を読み込ませることでそれがなんの曲かを当てることができるよ！")
 
-st.subheader("①15曲の中から好きな曲を選んでみよう！")
+default = Image.open("./figure/default.png")
+st.image(default, caption="コードくん", use_column_width=True)
+st.subheader("①15曲の中から好きな曲を選んでみよう!")
+
