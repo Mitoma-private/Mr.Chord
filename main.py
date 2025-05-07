76b88a82090b0cff0b252cd47c2ab0ea4de3401b
Mitoma-private <mitoayu1106@ezweb.ne.jp>
2025-05-07 06:37:25 +0000

    vv

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ import streamlit as st
 import numpy as np
 
 st.title("曲当てロボット コードくん")
-st.subheader("コードくんはピアノを引いた音源を読み込ませることでそれがなんの曲かを当てることができるよ！")
+st.text("コードくんはピアノを引いた音源を読み込ませることでそれがなんの曲かを当てることができるよ！")
+
+st.subheader("①15曲の中から好きな曲を選んでみよう！")
