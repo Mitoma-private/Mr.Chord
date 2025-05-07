dbfe655ad7d56879da48591c5561bef66cac8ece
Mitoma-private <mitoayu1106@ezweb.ne.jp>
2025-05-07 07:01:34 +0000

    bb

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ st.title("曲当てロボット コードくん")
 st.text("コードくんはピアノを引いた音源を読み込ませることでそれがなんの曲かを当てることができるよ！")
 
 default = Image.open("./figure/default.png")
-st.image(default, caption="コードくん")
-st.subheader("①15曲の中から好きな曲を選んでみよう!")
+happy = Image.open("./figure/happy.png")
+learning = Image.open("./figure/learning.png")
+sad = Image.open("./figure/sad.png")
+
+left, right = st.colmuns(2)
+right.image(default, caption="コードくん", width=100)
+left.subheader("①15曲の中から好きな曲を選んでみよう!")
 
