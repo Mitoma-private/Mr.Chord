65fe649ce20be98d58672885605967778a66c720
Mitoma-private <mitoayu1106@ezweb.ne.jp>
2025-05-08 15:55:25 +0000

    ff

--- config.py
+++ config.py
@@ -30,18 +30,43 @@ def Voice_content(num):
     return voice_html
 
 ##音声ファイル
-def Voice_content_songs(song_name):
-    path = "./Voice/song/"
-    voice_file = path+song_name+".wav"
+def Voice_content_songs(num, song_name):
+    if num < 10:
+        num = "0" + str(num)
+    else:
+        num = str(num)
+    path = "./Voice/"
+    voice_file = path+num+".wav"
+    song_file = path + "song/" + song_name + ".wav"
     with open(voice_file, "rb")as f:
         contents = f.read()
+    with open(song_file, "rb")as f:
+        contents_song = f.read()
         
     voice_str = "data:audio/ogg;base64,%s"%(base64.b64encode(contents).decode())
+    song_str = "data:audio/ogg;base64,%s"%(base64.b64encode(contents_song).decode())
+
+    mime_type = "audio/wav"
     voice_html = """
-                    <audio autoplay=True>
-                    <source src="%s" type="audio/ogg" autoplay=True>
-                    Your browser does not support the audio element.
+                    <audio id="audio1" autoplay=True>
+                        <source src="{voice_str}" type="audio/ogg" autoplay=True>
+                        Your browser does not support the audio element.
                     </audio> 
-                 """%voice_str
+                    
+                    <audio id="audio2">
+                        <source src="{song_str}" type="audio/ogg" autoplay=True>
+                        Your browser does not support the audio element.
+                    </audio> 
+                    
+                    <script>
+                        const audio1 = document.getElementById("audio1")
+                        const audio2 = document.getElementById("audio2")
+                        
+                        audio1.play();
+                        audio1.oneded = () => {{
+                            audio2.play();
+                        }}
+                    </script>
+                 """
 
     return voice_html
